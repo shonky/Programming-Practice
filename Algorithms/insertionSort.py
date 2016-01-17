993391287cd7d81acbd62dd4efe38607ef4e4728
shonky <sg10m@my.fsu.edu>
2016-01-16 19:37:52 -0500

    insertion sort started with basic example, make it so that u can send own comparision function in the future

---
+++ Algorithms/insertionSort.py
@@ -0,0 +1,46 @@
+class InsertionSort(object):
+	def __init__(self):
+		array = []
+		key = 1
+		temp = 1
+
+	array = []	
+	key = 1
+	temp = 1
+
+	def setArray(self, sArray):
+		self.array = sArray
+
+	def swap(self, keyI1, keyI2):
+		temp1 = self.array[keyI1]
+		self.array[keyI1] = self.array[keyI2]
+		self.array[keyI2] = temp1
+		temp1 = 0
+
+	def sort(self):
+		for i in range(len(self.array)-1):
+			if self.temp-1>=0 and self.temp<len(self.array):
+				if self.array[self.temp] < self.array[self.temp-1]:
+					print len(self.array)
+					self.swap(self.temp,self.temp-1)
+					self.temp = self.temp-1
+					self.sort()
+				else:
+						self.key = self.key + 1
+						self.temp = self.key
+			else:
+				self.key = self.key +1
+				self.temp = self.key
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	I1 = InsertionSort()
+	a = [5,4,3,2,1]
+	I1.setArray(a)
+	I1.sort()
+	print I1.array 
