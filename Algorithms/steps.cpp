6a5d3cf7b993a29ddd131f4815962540c48d70a4
shonky <sg10m@my.fsu.edu>
2016-02-18 17:14:21 -0500

    Create steps.cpp

---
+++ Algorithms/steps.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+int countSteps(int nSteps)
+{
+        static int nPoss = 0;
+
+        if(nSteps<1){
+                nPoss += 0;
+                return nPoss;
+        }
+        if(nSteps == 1){
+                nPoss += 1;
+        }
+        if(nSteps == 2){
+                nPoss += 2;
+        }
+        if(nSteps == 3){
+                nPoss += 3;
+        }
+
+        nPoss += countSteps(nSteps-3);
+
+        return nPoss;
+}
+
+int main()
+{
+        int numSteps = 0;
+        cout << "Please enter the number of steps:";
+        cin >> numSteps;
+        int numPoss= countSteps(numSteps);
+        cout << "Total Number of possibilities:" << numPoss <<endl;
+}
